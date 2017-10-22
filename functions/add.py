c8cd642f5961ea8415f2fbbeed6964b3b7f93647
Tim Rodger <tim.rodger@gmail.com>
2017-10-22 18:24:07 +0100

    Adds debug to add function

--- functions/add.py
+++ functions/add.py
@@ -1,12 +1,18 @@
 import json
-
+import boto3
 
 def handler(event, context):
 
+    id = event.pathParameters.id
+
     body = {
         "message": "add",
-        "input": event
+        "input": event,
+        "body": event.body,
+        "id": id
     }
+
+    client = boto3.client('dynamodb')
 
     response = {
         "statusCode": 200,
